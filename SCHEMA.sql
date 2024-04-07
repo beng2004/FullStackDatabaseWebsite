86960769c75e8fa2d74efe484e5b8129d27cc948
beng2004 <btguerrieri@gmail.com>
2024-04-07 14:18:56 -0400

    add schema

---
+++ SCHEMA.sql
@@ -0,0 +1,30 @@
+DROP TABLE GOAT;
+CREATE TABLE GOAT(
+	Goat_id integer primary key,
+	Gender varchar(20) NOT NULL default '',
+	Birth_date timestamp,
+	Breed varchar(20) NOT NULL default '',
+	Cohort_id integer,	
+	FOREIGN KEY (Cohort_id) REFERENCES COHORT (Cohort_id)
+);
+DROP TABLE WEIGH_IN;
+CREATE TABLE WEIGH_IN(
+	Weigh_in_date timestamp primary key,
+	Weight varchar(20) NOT NULL default '',
+	Goat_id integer,
+	FOREIGN KEY (Goat_id) REFERENCES GOAT (Goat_id)
+);
+DROP TABLE NOTE;
+	CREATE TABLE NOTE (
+	Goat_id integer NOT NULL,
+	Date_of_note timestamp,
+	Note varchar(30) NOT NULL,
+	primary key( animal_id, created )
+	FOREIGN KEY (Goat_id) REFERENCES GOAT (Goat_id)
+
+);
+DROP TABLE COHORT;
+CREATE TABLE COHORT (
+	Cohort_id integer primary key,
+	Cohort_name varchar(20),
+);
