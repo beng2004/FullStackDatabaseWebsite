aa26fbe47266819ce1fa5e393521ae045725355e
zorourke14 <zorourke14@gmail.com>
2024-04-23 14:50:21 -0400

    New API query for notes on singular goat

--- FullStack/queries.js
+++ FullStack/queries.js
@@ -3,43 +3,10 @@ const connectionString = 'postgres://lion:lion@localhost:5432/lion';
 const db = pgp(connectionString); 
 
 const singular = (request, response) => {
-  const startdate = request.query.startdate;
-  const enddate = request.query.enddate;
-  const startWeight = parseFloat(request.query.startWeight);
-  const endWeight = parseFloat(request.query.endWeight);
-  const Goat_idz = parseInt(request.query.Goat_idz)
-  const breed = request.query.breed;
-  const gender = request.query.gender;
+  const Goat_id = parseInt(request.query.Goat_id)
 
-
-  let query = 'SELECT WEIGH_IN.Weigh_in_date, WEIGH_IN.Weight, WEIGH_IN.Goat_id, GOAT.breed, GOAT.gender,GOAT.Birth_date, Note.Note FROM WEIGH_IN JOIN GOAT ON WEIGH_IN.Goat_id = GOAT.Goat_id JOIN NOTE ON Weigh_in.Goat_id = Note.Goat_id WHERE WEIGH_IN.Weigh_in_date > $1 AND WEIGH_IN.Weigh_in_date < $2'
-  let parameters = [startdate,enddate]
-
-
-if(startWeight != null) { //if front end indicates theres a start weight 
-  query += ' AND WEIGH_IN.Weight > $3'
-  parameters.push(startWeight)
-} 
-
-if(endWeight != null) { //if front end indicates theres an end weight 
-  query += ' AND WEIGH_IN.Weight < $' + (parameters.length + 1)
-  parameters.push(endWeight)
-} 
-
-if(!isNaN(Goat_idz)) { //if front end indicates theres an end weight 
-  query += ' AND WEIGH_IN.Goat_id = $' + (parameters.length + 1)
-  parameters.push(Goat_idz)
-} 
-
-if(breed  != null) { //if front end indicates theres a breed 
-  query += ' AND GOAT.breed = $' + (parameters.length + 1)
-  parameters.push(breed)
-} 
-
-if(gender != null) { //if front end indicates theres a gender 
-  query += ' AND GOAT.gender = $' + (parameters.length + 1)
-  parameters.push(gender)
-} 
+  let query = 'SELECT Goat_id, Note FROM NOTE WHERE NOTE.Goat_id = $1'
+  let parameters = [Goat_id]
 
   db.any(query , parameters)
   .then(data => {
@@ -56,6 +23,7 @@ const originalQuery = (request, response) => {
   const enddate = request.query.enddate;
   const startWeight = parseFloat(request.query.startWeight);
   const endWeight = parseFloat(request.query.endWeight);
+  const Goat_id = parseFloat(request.query.Goat_id);
   const gender = request.query.gender;
   const breed = request.query.breed;
 
@@ -73,6 +41,11 @@ if(endWeight != null) { //if front end indicates theres an end weight
   parameters.push(endWeight)
 } 
 
+if(!isNaN(Goat_id)) { //if front end indicates theres a goat_id 
+  query += ' AND GOAT.Goat_id = $' + (parameters.length + 1)
+  parameters.push(Goat_id)
+} 
+
 if(gender != null) { //if front end indicates theres a gender 
   query += ' AND GOAT.gender = $' + (parameters.length + 1)
   parameters.push(gender)
@@ -82,6 +55,7 @@ if(breed  != null) { //if front end indicates theres a breed
   query += ' AND GOAT.breed = $' + (parameters.length + 1)
   parameters.push(breed)
 } 
+
 
   db.any(query , parameters)
   .then(data => {
